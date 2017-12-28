78a082fa92a6dd1fce61b33ee84a9b7b9bdff59e
warlock921 <l>
2017-12-28 18:37:06 +0800

    增加文章标签添加、显示功能

--- article/views.py
+++ article/views.py
@@ -16,6 +16,8 @@ from django.core.files.base import ContentFile
 from .forms import AriticleColumnForm,AriticlePostForm,ArticleTagForm
 from .models import AriticleColumn,AriticlePost,ArticleTag
 
+import json
+
 #话题标签视图
 @login_required(login_url='/account/login')
 @csrf_exempt
@@ -28,6 +30,7 @@ def article_column(request):
 	if request.method == "POST":
 		column_name = request.POST['column']
 		columns = AriticleColumn.objects.filter(user_id=request.user.id,column=column_name)
+		#取得当前用户的所有栏目结果集
 		columns_re = AriticleColumn.objects.filter(user=request.user)
 		if columns:
 			return HttpResponse('2')
@@ -70,6 +73,8 @@ def del_article_column(request):
 @login_required(login_url='/account/login')
 @csrf_exempt
 def article_post(request):
+	tag_list = ArticleTag.objects.filter(author=request.user)
+	#print(tag_list)
 	if request.method == "POST":
 		article_post_form = AriticlePostForm(data=request.POST)
 		if article_post_form.is_valid():
@@ -79,6 +84,13 @@ def article_post(request):
 				new_article.author = request.user
 				new_article.column = request.user.article_column.get(id=request.POST['column_id'])
 				new_article.save()
+
+				tags = request.POST['tags']
+				if tags:
+					for atag in json.loads(tags):
+						tag = request.user.tag.get(tag=atag) 
+						new_article.article_tag.add(tag)
+
 				return HttpResponse("1")
 			except Exception as e:
 				return HttpResponse("2")
@@ -87,7 +99,8 @@ def article_post(request):
 	else:
 		article_post_form = AriticlePostForm()
 		article_columns = request.user.article_column.all()
-		return render(request, "article/column/article_post.html", {"article_post_form":article_post_form, "article_columns":article_columns})
+		#article_tags = request.user.tag.all()
+		return render(request, "article/column/article_post.html", {"article_post_form":article_post_form, "article_columns":article_columns, "tag_list":tag_list})
 
 #已发布话题列表视图
 @login_required(login_url='/account/login')
@@ -159,7 +172,7 @@ def editor_image_upload(request):
 		#取文件的扩展名
 		file_ext = os.path.splitext(upload_image_file.name)[1]
 		new_upload_image_file = timezone.now().strftime('%Y%m%d%H%M%S') + file_ext
-		print(new_upload_image_file)
+		#print(new_upload_image_file)
 		if not upload_image_file:
 			response_data['success'] = 0
 			response_data['message'] = u'图片格式异常'
@@ -196,19 +209,36 @@ def article_tag(request):
 		return render(request, "article/tag/tag_list.html", {"article_tags":article_tags, "article_tag_form":article_tag_form})
 
 	if request.method == "POST":
-		tag_post_form = ArticleTagForm(data=request.POST)
-		if tag_post_form.is_valid():
-			print("表单验证通过！")
-			try:
-				new_tag = tag_post_form.save(commit=False)
-				new_tag.author = request.user
-				new_tag.save()
-				return HttpResponse("1")
-			except Exception as e:
-				print(e)
-				return HttpResponse("数据保存失败")
-			else:
-				return HttpResponse("对不起，表单验证失败！")
+		#取得用户输入的tag名称
+		tag_name = request.POST['tag']
+		#从数据库中筛选与当前author_id、tag相同的记录
+		tags = ArticleTag.objects.filter(author_id=request.user.id,tag=tag_name)
+		#从数据库中筛选当前用户的所有tag
+		tag_re = ArticleTag.objects.filter(author=request.user)
+
+		#首先判断tag是否存在
+		if tags:
+			return HttpResponse('标签名称已存在，请重新更换标签名称！')
+		#如果tag不存在，就判断用户的tag数量是否超过10个（含10个），超出则不允许再创建
+		elif(tag_re.count()>=10):
+			return HttpResponse('标签数量超过10个，无法再添加！')
+		#如果前两个条件均不满足，则创建新tag
+		else:
+			#下面是创建tag的过程
+			tag_post_form = ArticleTagForm(data=request.POST)
+			print(tag_re)
+			if tag_post_form.is_valid():
+				print("表单验证通过！")
+				try:
+					new_tag = tag_post_form.save(commit=False)
+					new_tag.author = request.user
+					new_tag.save()
+					return HttpResponse("1")
+				except Exception as e:
+					print(e)
+					return HttpResponse("数据保存失败")
+				else:
+					return HttpResponse("对不起，表单验证失败！")
 
 #删除话题标签视图
 @login_required(login_url='/account/login')
