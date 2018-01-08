1de3206e92afe380b38155f6ec8be19180482a70
warlock921 <l>
2018-01-08 11:38:03 +0800

    完善用户流功能，小BUG修改

--- account/admin.py
+++ account/admin.py
@@ -4,10 +4,12 @@ from .models import UserProfile,UserInfo
 class UserProfileAdmin(admin.ModelAdmin):
 	list_display = ('user','birth','phone')
 	list_filter = ("phone",)
+	search_fields = ['phone']
 
 class UserInfoAdmin(admin.ModelAdmin):
 	list_display = ('user','company','profession','address','aboutme')
 	list_filter = ("company",)
+	search_fields = ['company']
 		
 
 admin.site.register(UserProfile,UserProfileAdmin)
--- actions/admin.py
+++ actions/admin.py
@@ -4,5 +4,5 @@ from .models import Action
 class ActionAdmin(admin.ModelAdmin):
 	list_display = ('user','verb','target','created')
 	list_filter = ('created',)
-	search_filter = ('verb',)
+	search_fields = ['verb']
 admin.site.register(Action,ActionAdmin)
